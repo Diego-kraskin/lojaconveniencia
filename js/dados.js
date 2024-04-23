4ea802535312a57ad4682709994a51cbf216a3b8
Diego-kraskin <115913465+Diego-kraskin@users.noreply.github.com>
2024-04-23 00:34:39 -0300

    Add files via upload

---
+++ js/dados.js
@@ -0,0 +1,541 @@
+var MENU = {
+    "churrasco": [
+        {
+            "id": "ribs-brisket-and-burnt-ends",
+            "img": "./img/cardapio/churrasco/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg",
+            "name": "Joe's KC BBQ",
+            "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
+            "price": 110.99
+        },
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "./img/cardapio/churrasco/carolina-bbq-oink-sampler.1340b5a10cedc238cb2280306dd1d5a5.jpg",
+            "name": "Kings BBQ",
+            "dsc": "Carolina BBQ Oink Sampler",
+            "price": 89
+        },
+        {
+            "id": "texas-monthlys-1-bbq-brisket",
+            "img": "./img/cardapio/churrasco/texas-monthlys-1-bbq-brisket.1006a061be7acae03992e420fbca995a.jpg",
+            "name": "Snow's BBQ",
+            "dsc": "Texas Monthly's #1 BBQ Brisket",
+            "price": 199
+        },
+        {
+            "id": "whole-brisket-texas-bbq-sauce",
+            "img": "./img/cardapio/churrasco/whole-brisket-texas-barbecue-bbq-sauce.e07ee4818b20ed43d217bf67fedd41ce.jpg",
+            "name": "Franklin Barbecue",
+            "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
+            "price": 249
+        },
+        {
+            "id": "whole-texas-smoked-brisket",
+            "img": "./img/cardapio/churrasco/whole-texas-smoked-brisket.a5558a25381e271408e197936e7985d8.jpg",
+            "name": "Terry Black's Barbecue",
+            "dsc": "Whole Texas Smoked Brisket",
+            "price": 189
+        },
+        {
+            "id": "mini-trinity-bbq-combo-brisket-ribs-and-links",
+            "img": "./img/cardapio/churrasco/mini-trinity-bbq-combo-brisket-ribs-and-links.245582f593bf64b23b57dfca2be18cfd.jpg",
+            "name": "Bludso's BBQ",
+            "dsc": "Mini Trinity BBQ Combo - Brisket, Ribs & Links",
+            "price": 139
+        },
+        {
+            "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
+            "img": "./img/cardapio/churrasco/blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo.a95a3e632ae324f719738a2a5c1dff6e.jpg",
+            "name": "Blue Smoke",
+            "dsc": "Blue Smoke Baby Back Ribs + Backyard Barbecue Chicken Combo",
+            "price": 129
+        },
+        {
+            "id": "006-kings-meat-lovers-special",
+            "img": "./img/cardapio/churrasco/bbq-meat-lovers-special-for-10.36ca670fda4bfa783c2ea9165e068d26.jpg",
+            "name": "Kings BBQ",
+            "dsc": "BBQ Meat Lovers Special for 10",
+            "price": 139
+        },
+        {
+            "id": "the-big-ugly-bbq-dinner-for-6",
+            "img": "./img/cardapio/churrasco/the-big-ugly-bbq-dinner-for-6.2dfae7818811adddce85cc1a910881a0.jpg",
+            "name": "Ugly Drum",
+            "dsc": "The Big Ugly BBQ Dinner for 6",
+            "price": 229
+        },
+        {
+            "id": "17796-mighty-quinns-bbq-sampler-pack",
+            "img": "./img/cardapio/churrasco/mighty-quinns-bbq-sampler-pack.1bfe4a0665edc565756f5241bf25840e.jpg",
+            "name": "Mighty Quinn's BBQ",
+            "dsc": "Mighty Quinn's BBQ Sampler Pack",
+            "price": 169
+        },
+        {
+            "id": "post-oak-smoked-half-brisket",
+            "img": "./img/cardapio/churrasco/post-oak-smoked-usda-prime-half-brisket.6f17178dde7d806670bcc73ff11762b3.jpg",
+            "name": "Southside Market & Barbeque",
+            "dsc": "Post Oak Smoked USDA Prime Half Brisket",
+            "price": 109
+        },
+        {
+            "id": "best-of-texas-bbq-combo-serves-14",
+            "img": "./img/cardapio/churrasco/best-of-texas-bbq-combo-serves-14.7ae90266335e539c67e77fed14b43029.jpg",
+            "name": "Snow's BBQ",
+            "dsc": "Best of Texas BBQ Combo - Serves 14",
+            "price": 269
+        }
+    ],
+    "burgers": [
+        {
+            "id": "the-gramercy-tavern-burger-4-pack",
+            "img": "images/kit-natasha.jfif",
+            "name": "Kit Natasha",
+            "dsc": "The Gramercy Tavern Burger - 4 Pack",
+            "price": 99
+        },
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "images/kit-big.jfif",
+            "name": "Kit Big Apple",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 49
+        },
+        
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "images/kit-big.jfif",
+            "name": "Kit Big Apple",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 49
+        },
+
+        {
+            "id": "the-gramercy-tavern-burger-4-pack",
+            "img": "images/kit-natasha.jfif",
+            "name": "Kit Natasha",
+            "dsc": "The Gramercy Tavern Burger - 4 Pack",
+            "price": 99
+        },
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "images/kit-big.jfif",
+            "name": "Kit Big Apple",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 49
+        },
+        
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "images/kit-big.jfif",
+            "name": "Kit Big Apple",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 49
+        },
+
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "images/kit-big.jfif",
+            "name": "Kit Big Apple",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 49
+        },
+        
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "images/kit-big.jfif",
+            "name": "Kit Big Apple",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 49
+        },
+    ],
+    "sobremesas": [
+        {
+            "id": "15259-german-chocolate-killer-brownie-tin-pack",
+            "img": "./img/cardapio/sobremesas/german-chocolate-killer-brownie-tin-pack.5ebc34160f28767a9d94c4da2e04c4b9.jpg",
+            "name": "Killer Brownie®",
+            "dsc": "German Chocolate Killer Brownie®",
+            "price": 39.99
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies",
+            "img": "./img/cardapio/sobremesas/jacques-world-famous-chocolate-chip-cookies-6-pack.2217a14c443602493bba88aa9335319a.jpg",
+            "name": "Jacques Torres Chocolate",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 6 Pack",
+            "price": 39.95
+        },
+        {
+            "id": "luigis-original-cannoli-pie",
+            "img": "./img/cardapio/sobremesas/original-cannoli-pie.4cb5b9ba82f57b69b90765fd9f07aa1a.jpg",
+            "name": "The Cannoli Pie Company",
+            "dsc": "Original Cannoli Pie",
+            "price": 69
+        },
+        {
+            "id": "sea-salted-caramel-swirl-cheesecake",
+            "img": "./img/cardapio/sobremesas/sea-salted-caramel-swirl-cheesecake.e2825335433fb7a272a5d77649a6849e.jpg",
+            "name": "Cotton Blues Cheesecake Company",
+            "dsc": "Sea-Salted Caramel Swirl Cheesecake",
+            "price": 65
+        },
+        {
+            "id": "brooklyn-blackout-cookie-brownie-combo-pack-2-tins",
+            "img": "./img/cardapio/sobremesas/brooklyn-blackout-cookie-brownie-combo-pack-2-tins.d8805325baf6b23b4f01d119dc4531a7.jpg",
+            "name": "Brooklyn Blackout Company",
+            "dsc": "Brooklyn Blackout Cookie + Brownie Combo Pack - 2 Tins",
+            "price": 89
+        },
+        {
+            "id": "best-seller-cupcake-dozen",
+            "img": "./img/cardapio/sobremesas/best-seller-cupcake-dozen.f93b21993f6a2da11c975d45b9b0d08f.jpg",
+            "name": "Crave Cupcakes",
+            "dsc": "Best Seller Cupcake Dozen",
+            "price": 89
+        },
+        {
+            "id": "choose-your-own-ice-cream-donuts-6-pack",
+            "img": "./img/cardapio/sobremesas/choose-your-own-ice-cream-donuts-6-pack.24d0b44765a7c54237fcd7ea9d9d8093.jpg",
+            "name": "Elegant Desserts",
+            "dsc": "Choose Your Own Ice Cream Donuts - 6 Pack",
+            "price": 69
+        },
+        {
+            "id": "17481-jewish-dessert-3-pack",
+            "img": "./img/cardapio/sobremesas/jewish-classics-dessert-pack.7d4b76630f2fe75dcb7bbcf2501b7390.jpg",
+            "name": "Ess-a-Bagel",
+            "dsc": "Jewish Classics Dessert Pack",
+            "price": 89.95
+        },
+        {
+            "id": "dessert-bar-care-package",
+            "img": "./img/cardapio/sobremesas/dessert-bar-care-package.324aa28fe98c9dc67b75aac43376902e.jpg",
+            "name": "Bread and Roses Bakery",
+            "dsc": "Dessert Bar Care Package",
+            "price": 65
+        },
+        {
+            "id": "donut-cookies-12-pack",
+            "img": "./img/cardapio/sobremesas/donut-cookies-12-pack.46f42c61c4a79fd2051a99b2f23e890e.jpg",
+            "name": "Stan's Donuts",
+            "dsc": "Donut Cookies - 12 Pack",
+            "price": 49
+        },
+        {
+            "id": "gulab-jamun-ice-cream-cakes-2-pack",
+            "img": "./img/cardapio/sobremesas/gulab-jamun-ice-cream-cakes-2-pack.c45b4d0750ad22d741f84dc1f26d20e7.jpg",
+            "name": "Malai Ice Cream",
+            "dsc": "Gulab Jamun Ice Cream Cakes - 2 Pack",
+            "price": 79
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies-12-pack",
+            "img": "./img/cardapio/sobremesas/jacques-world-famous-chocolate-chip-cookies-12-pack.3b373bdd67cd25084182c21499f675d1.jpg",
+            "name": "Jacques Torres Chocolate",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 12 Pack",
+            "price": 69.95
+        }
+    ],
+    "bebidas": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "images/vinhos.jfif",
+            "name": "vinho",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "images/vinhos.jfif",
+            "name": "vinho",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "images/vinhos.jfif",
+            "name": "vinho",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "images/vinhos.jfif",
+            "name": "vinho",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "images/vinhos.jfif",
+            "name": "vinho",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "images/vinhos.jfif",
+            "name": "vinho",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "images/vinhos.jfif",
+            "name": "vinho",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "images/vinhos.jfif",
+            "name": "vinho",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "images/vinhos.jfif",
+            "name": "vinho",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+       
+       
+       
+    ],
+    "pizzas": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/skol-lata-269.jfif", 
+            "name": "Skol Beats 269ml",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 8.99 
+        },
+       
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/skol-lata-269.jfif", 
+            "name": "Skol Beats 269ml",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 8.99 
+        },
+
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/skol-lata-269.jfif", 
+            "name": "Skol Beats 269ml",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 8.99 
+        },
+
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/skol-lata-269.jfif", 
+            "name": "Skol Beats 269ml",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 8.99 
+        },
+       
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/skol-lata-269.jfif", 
+            "name": "Skol Beats 269ml",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 8.99 
+        },
+
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/skol-lata-269.jfif", 
+            "name": "Skol Beats 269ml",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 8.99 
+        },
+
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/skol-lata-269.jfif", 
+            "name": "Skol Beats 269ml",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 8.99 
+        },
+       
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/skol-lata-269.jfif", 
+            "name": "Skol Beats 269ml",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 8.99 
+        },
+
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/skol-lata-269.jfif", 
+            "name": "Skol Beats 269ml",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 8.99 
+        },
+
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/skol-lata-269.jfif", 
+            "name": "Skol Beats 269ml",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 8.99 
+        },
+       
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/skol-lata-269.jfif", 
+            "name": "Skol Beats 269ml",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 8.99 
+        },
+
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/skol-lata-269.jfif", 
+            "name": "Skol Beats 269ml",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 8.99 
+        },
+    ],
+    "steaks": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+
+         {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./images/coca-2l.jfif",
+            "name": "Coca cola 2lts",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+        
+        
+    ]
+}
